44143aa44f3e07ce81d2c688348c92c94318c68f
seizethedaylyn <22000189@handong.ac.kr>
2023-10-11 03:09:20 +0900

    231011 test commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const name1 = document.getElementById('name1');
+name1.value = "김채린";
+
+const email1 = document.getElementById('email1');
+email1.value = "asdf@naver.com";
+
+const pnumber1 = document.getElementById('pnumber1');
+pnumber1.value = "01012345678";
+
+const number1 = document.getElementById('number1');
+number1.value = "22000189";
+
+const select1 = document.getElementById('select1');
+select1.options[2].selected = true;
+const select2 = document.getElementById('select2');
+select2.options[7].selected = true;
+
+const select3 = document.getElementById('select3');
+select3.options[3].selected = true;
+const select4 = document.getElementById('select4');
+select4.options[17].selected = true;
+
+const select5 = document.getElementById('select5');
+select5.options[4].selected = true;
+const select6 = document.getElementById('select6');
+select6.options[16].selected = true;
+
+const date = document.getElementById('Date');
+date.value = "2023-10-15";
+
+const content = document.getElementById('textarea1');
+content.value = "지금 준비하고 있는 프로젝트 컨펌 받고 싶습니다.";
